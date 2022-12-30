b5fa4337ad563e6f07e8d174d2e6149a848094a6
kartikjain-sudo <kartikjain42@protonmail.com>
2022-12-30 16:42:05 +0530

    feat: array sum3calldata

--- contracts/sumOfArray.sol
+++ contracts/sumOfArray.sol
@@ -52,4 +52,15 @@ contract sum {
             }
         }
     }
+
+    function sumLL3calldata(uint256[] calldata _arr) public pure returns (uint256 _sum) { // 23145 gas
+        // _arr is the pointer in memory where the length of the array is stored
+        assembly {
+            // let len := _arr.length // Load the length of the array from the first slot
+            let end := add(_arr.offset, mul(0x20, _arr.length)) 
+            for {let i := _arr.offset } iszero(eq(i,end)) { i:= add(i,0x20)} { // Start at i = 0 and iterate while i < len, incrementing i by 1 each time
+                _sum := add(_sum, calldataload(i)) // Add the value at the current index to the sum
+            }
+        }
+    }
 }
